43d61538566f1f20ab6375154924f3461fafdd7b
JorisPaarde <jorispaarde@gmail.com>
2021-10-05 08:14:27 +0000

    Added card selection

--- battle/urls.py
+++ battle/urls.py
@@ -6,4 +6,5 @@ app_name = "battle"
 urlpatterns = [
     path('', views.battle_screen, name='battle-screen'),
     path('<game>', views.battle_screen, name='battle-screen'),
+    path('card_select/<card>', views.card_select, name='card-select'),
 ]
--- battle/views.py
+++ battle/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Player, Game, Current_game_floor, Enemy, Game_floor_enemy
 from profiles.models import Card
 from django.contrib import messages
@@ -15,6 +15,7 @@ def battle_screen(request, game):
     game = Game.objects.get(player=player)
     current_game_floor = Current_game_floor.objects.get(pk=game.current_game_floor.pk)
 
+    # card playing phase
     if game.game_step == '1':
         messages.info(request, "Please select a card to play.")
 
@@ -26,3 +27,16 @@ def battle_screen(request, game):
     }
 
     return render(request, 'battle/battle.html', context)
+
+
+def card_select(request, card):
+
+    current_user = request.user
+    player = Player.objects.get(user=current_user)
+    game = Game.objects.get(player=player)
+
+    if request.method == 'POST':
+
+        print(card)
+
+    return redirect('battle:battle-screen', game)
