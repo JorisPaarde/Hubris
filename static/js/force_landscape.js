542d64f42ab51cc0034865a2b39261ee70a6cf51
JorisPaarde <jorispaarde@gmail.com>
2021-10-12 14:35:27 +0000

    Refactored function

--- static/js/force_landscape.js
+++ static/js/force_landscape.js
@@ -1,7 +1,5 @@
-$( window ).on( "load", function() {
-    if ((window.innerHeight > 100) && (window.innerWidth > 100)){
-        $(".landscape-screen").toggleClass( 'd-none', (window.innerHeight < window.innerWidth))
-    };
+$( window ).on( "load", function() { 
+     $(".landscape-screen").toggleClass( 'd-none', (window.innerHeight < window.innerWidth));
 });
 
 window.addEventListener("orientationchange", function() {
