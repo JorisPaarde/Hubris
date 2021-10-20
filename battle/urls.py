c2d706d1d9bfd78401ecfb612d92aa905239d1d6
JorisPaarde <jorispaarde@gmail.com>
2021-10-20 20:50:46 +0000

    removed redundant url

--- battle/urls.py
+++ battle/urls.py
@@ -8,7 +8,6 @@ urlpatterns = [
     path('', views.battle_screen, name='battle-screen'),
     path('<game>', views.battle_screen, name='battle-screen'),
     path('card_select/<card>', views.card_select, name='card-select'),
-    path('action_processor', utils.action_processor, name='action_processor'),
     path('proceed-to-next-floor/', views.proceed_to_next_floor, name='proceed_to_next_floor'),
     path('next-floor-start/<choice>', views.next_floor_start, name='next-floor-start'),
 ]
