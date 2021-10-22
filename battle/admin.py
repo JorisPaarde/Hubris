442a67f7e77ff4a82e6327dc11b64c4b87b3def8
JorisPaarde <jorispaarde@gmail.com>
2021-10-22 08:34:06 +0000

    Added enemy in free version display

--- battle/admin.py
+++ battle/admin.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
-from .models import *
+from .models import Enemy, Game, Current_game_floor, Game_floor_enemy
 # Register your models here.
 
-admin.site.register(Enemy)
+
+class EnemyyAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'in_freeversion',
+    )
+
+admin.site.register(Enemy, EnemyyAdmin)
 admin.site.register(Game)
 admin.site.register(Current_game_floor)
 admin.site.register(Game_floor_enemy)
